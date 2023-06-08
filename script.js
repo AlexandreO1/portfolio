d0d996ce2dafae1b128496ca84b234083df61075
AlexandreO1 <alexandre.oualid@epitech.eu>
2023-06-08 17:33:22 +0200

    add responsive

---
+++ script.js
@@ -0,0 +1,16 @@
+// Fonction pour basculer la visibilité de l'image en fonction du bouton cliqué
+function toggleImage(event) {
+  var target = event.target.getAttribute("data-target");
+  var image = document.getElementById(target);
+  if (image.style.display === "none") {
+    image.style.display = "block";
+  } else {
+    image.style.display = "none";
+  }
+}
+
+// Ajouter un gestionnaire d'événement à tous les boutons avec la classe "button"
+var buttons = document.getElementsByClassName("button");
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", toggleImage);
+}
